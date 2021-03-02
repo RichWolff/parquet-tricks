be57ae8b0ea41220b8934a39eb68eb54bd74dc1a
Richard Wolff <richwolff12@gmail.com>
2021-03-02 09:10:40 -0500

    Update sqlalchemy_ORM_Model.py

--- sqlalchemy_ORM_Model.py
+++ sqlalchemy_ORM_Model.py
@@ -78,3 +78,73 @@ class Observations(Base):
 
     def __repr__(self):
         return f"<Observations(cycle_id={self.cycle_id}, TimeStamp={self.TimeStamp}, normalized_time={self.normalized_time}, tag={self.tag}, value={self.value})>"  # noqa
+    
+
+# Utilities for writing to sql
+def pandasIter(df: pd.DataFrame, chunksize: int) -> Generator[pd.DataFrame, None, None]:
+    """Iterates through a pandas dataframe using a generator framework at `chunksize` rows.
+
+    Args:
+        df (pd.DataFrame): [description]
+        chunksize (int): [description]
+
+    Yields:
+        Generator[pd.DataFrame, None, None]: The original pandas dataframe chunked by chunksize
+    """
+    iters = df.shape[0] // chunksize + 1 if df.shape[0] >= chunksize else 2
+    rge = np.linspace(0, df.shape[0], iters, dtype=np.int)
+    for i, j in zip(rge, rge[1:]):
+        yield df.iloc[i:j]
+
+
+def to_records(df: pd.DataFrame) -> List[Dict[str, str]]:
+    """Return pandas dataframe as a list of records. Replaces np.nan types with None.
+
+    Args:
+        df (pd.DataFrame): Dataframe to convert to records
+
+    Returns:
+        List[Dict[str, str]]: original dataframe in [{'column': 'value'}] form.
+    """
+    columns = df.columns.tolist()
+    return [
+        dict(zip(columns, [None if pd.isnull(r) else r for r in row]))
+        for row in df.itertuples(index=False, name=None)
+    ]
+
+
+async def _async_to_sql(df: pd.DataFrame, table: SQLALCHEMY_TABLE, conn: ASYNC_TRANSACTION, chunksize: int = 1000) -> None:
+    """Asynchronously iterates through chunks of a dataframe and sends it to sql using SQLALCHEMY and asyncio.
+
+    Args:
+        df (pd.DataFrame): Dataframe to iterate through
+        table (SQLALCHEMY_TABLE): Table object that data will pass through
+        conn (ASYNC_TRANSACTION): Sql alchemy Async Transaction. If one fails, all data will fail to push.
+        chunksize (int, optional): # Of Rows to iterate through per batch. Defaults to 1000.
+    """
+    await asyncio.gather(*[
+        asyncio.create_task(conn.execute(
+            table.__table__.insert().values(to_records(df_iter))
+        )) for df_iter in pandasIter(df, chunksize)
+    ])
+
+
+async def async_to_sql(test: ProactiveMonitoring, chunksize: int = 1000) -> None:
+    """Asynchronously iterates through the header, metric, and observations table of our proactive monitoring,
+    then chunks each dataframe and sends it to sql using SQLALCHEMY and asyncio.
+
+    Args:
+        test (ProactiveMonitoring): [description]
+        chunksize (int, optional): [description]. Defaults to 1000.
+    """
+    _metrics = test.metrics.reset_index()
+    _header = test.header.rename(columns={'value': 'missing_metrics'}).reset_index()
+    _observations = test.observations.copy()
+    _observations.columns.name = 'tag'
+    _observations = _observations.stack().to_frame('value').reset_index()
+
+    async with ASYNC_ENGINE.begin() as conn:
+        await _async_to_sql(_header, Headers, conn, chunksize=chunksize)
+        await _async_to_sql(_metrics, Metrics, conn, chunksize=chunksize)
+        await _async_to_sql(_observations, Observations, conn, chunksize=chunksize)
+
